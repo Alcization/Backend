a2c092d4f2bf8dbfd4ab7d73404eb1b3597e54c3
Alcization <loc312004@gmail.com>
2026-05-03 14:57:16 +0700

    Adjust endpoint

--- src/routes/openweather.routes.js
+++ src/routes/openweather.routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const openWeatherCtrl = require('../controllers/openweather.controller');
 
 // Public proxy endpoints for OpenWeather APIs
-router.post('/geo/1.0/direct', openWeatherCtrl.getDirectGeocoding);
-router.post('/data/2.5/forecast', openWeatherCtrl.getForecast);
-router.post('/data/2.5/weather', openWeatherCtrl.getCurrentWeather);
-router.post('/data/2.5/history/city', openWeatherCtrl.getCityHistory);
+router.post('/geo', openWeatherCtrl.getDirectGeocoding);
+router.post('/data/forecast', openWeatherCtrl.getForecast);
+router.post('/data/weather', openWeatherCtrl.getCurrentWeather);
+router.post('/data/history/city', openWeatherCtrl.getCityHistory);
 
 module.exports = router;
